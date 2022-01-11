0271b2d54b191e36e874fb98ec75b2062526a0e1
rafsousa <rodrigo.alex.sousa@gmail.com>
2022-01-11 16:57:55 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,10 @@
+import Pagination from "components/Pagination";
 
 function Listing() {
     return (
-        <h1>TESTE LISTING</h1>
+        <div>
+            <Pagination />
+        </div>
     )
 }
 
